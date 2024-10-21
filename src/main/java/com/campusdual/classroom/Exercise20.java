3808a1486dc2d3e45d8b9792a7d6208d8185cdb0
Daniel <dani.v2509@outlook.com>
2024-10-21 15:21:01 +0200

    Solution

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,11 +6,22 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
+        // Creamos una lista de <elementos> tipo "Person"
+        List<Person> peopleList = new ArrayList<>();
 
+        // Añadimos los elementos necesarios (instrucciones ej)
+        peopleList.add(new Person("John", "Smith"));
+        peopleList.add(new Teacher("María", "Montessori", "Educación"));
+        peopleList.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        peopleList.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
+
+        return peopleList;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        for (Person p: stringList){
+            p.getDetails();
+        }
     }
 
     public static void main(String[] args) {
